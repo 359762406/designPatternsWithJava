5ae806c7fc9056b1021d04f35ed14e9831a2f211
359762406 <359762406@qq.com>
2023-09-01 16:00:02 +0800

    designpatterns

--- src/com/itheima/pattern/prototype/test1/CitaionTest.java
+++ src/com/itheima/pattern/prototype/test1/CitaionTest.java
@@ -11,7 +11,7 @@ import java.io.ObjectOutputStream;
  * @Description: TODO(一句话描述该类的功能)
  * @Author: 黑马程序员
  */
-public class CitaionTest {
+public class  CitaionTest {
     public static void main(String[] args) throws Exception {
         //1，创建原型对象
         Citation citation = new Citation();
@@ -21,14 +21,14 @@ public class CitaionTest {
         citation.setStu(stu);
 
         //创建对象输出流对象
-        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("d:/robin/a.txt"));
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("a.txt"));
         //写对象
         oos.writeObject(citation);
         //释放资源
         oos.close();
 
         //创建对象输入流对象
-        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("d:/robin/a.txt"));
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("a.txt"));
         //读取对象
         Citation citation1 = (Citation) ois.readObject();
         //释放资源
